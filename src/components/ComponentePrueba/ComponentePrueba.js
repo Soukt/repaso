296635f0266a74f4a4daf682337ce467ac1cd31d
Soukt <mtorresfelgueroso@gmail.com>
2020-10-10 12:23:50 +0200

    commit sabado

---
+++ src/components/ComponentePrueba/ComponentePrueba.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+export class ComponentePrueba extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      age: this.props.edad,
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        ***********************
+        {/* <h1>{this.state.age}</h1>
+        <h1>el nombre es: {this.props.user.nombre}</h1> */}
+        <h2>La edad {this.state.age}</h2>
+        ************************
+      </div>
+    );
+  }
+}
+
+export default ComponentePrueba;
